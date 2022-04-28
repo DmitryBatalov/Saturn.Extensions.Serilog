a2712b396169a8477871f9b2696998be53d20879
dmitry <dmitry@functor.team>
2022-04-28 18:35:19 +0400

    Bump version to 0.1.0
    ## [0.1.0] - 2022-04-28
    
    First release
    
    ### Added
    - The initial functionality has added.

--- src/Saturn.Extensions.Serilog/AssemblyInfo.fs
+++ src/Saturn.Extensions.Serilog/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Saturn.Extensions.Serilog")>]
 [<assembly: AssemblyProductAttribute("Saturn.Extensions.Serilog")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-04-28T00:00:00.0000000+04:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","4a347b9c9334e4c9809f1f5c31d587af8b9be7b5")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Saturn.Extensions.Serilog"
     let [<Literal>] AssemblyProduct = "Saturn.Extensions.Serilog"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-04-28T00:00:00.0000000+04:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "4a347b9c9334e4c9809f1f5c31d587af8b9be7b5"
